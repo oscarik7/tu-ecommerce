c17a2c0d49dafb896503cc119080733aa439a29a
oscarik7 <devparaguay@gmail.com>
2025-12-18 15:58:38 -0300

    modificacion de logo

--- resources/views/livewire/components/navbar.blade.php
+++ resources/views/livewire/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="bg-white shadow-lg sticky top-0 z-50">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="w-full">
         <div class="flex justify-between h-16">
             <div class="flex items-center">
                 <div class="flex items-center">
